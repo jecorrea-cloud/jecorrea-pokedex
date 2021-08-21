ff09226e1c0db70844fcd1754fc46d8f8ef93094
joaquin <jecorrea@protonmail.com>
2021-08-21 15:35:08 -0400

    Almost done with JS

--- index.js
+++ index.js
@@ -1,75 +1,94 @@
 //Selecting Card's components
-const pokeCard = document.querySelector('[card]');
-const pokeId = document.querySelector('[id]');
-const pokeImgContainer = document.querySelector('[img-container]');
-const pokeImg = document.querySelector('[img]');
-const pokeName = document.querySelector('[poke-name]');
-const pokeTypes = document.querySelector('[types]');
-const pokeWeight = document.querySelector('[weight]');
-const pokeHeight = document.querySelector('[height]');
-const pokeStats = document.querySelector('[stats]');
+const pokeCard = document.querySelector("[card]");
+const pokeId = document.querySelector("[id]");
+const pokeImgContainer = document.querySelector("[img-container]");
+const pokeImg = document.querySelector("[img]");
+const pokeName = document.querySelector("[poke-name]");
+const pokeTypes = document.querySelector("[types]");
+const pokeWeight = document.querySelector("[weight]");
+const pokeHeight = document.querySelector("[height]");
+const pokeStats = document.querySelector("[stats]");
 
 // Dictionary for colors based on types
-const typeColors = {
-        electric: '#FFEA70',
-    normal: '#B09398',
-    fire: '#FF675C',
-    water: '#0596C7',
-    ice: '#AFEAFD',
-    rock: '#999799',
-    flying: '#7AE7C7',
-    grass: '#4A9681',
-    psychic: '#FFC6D9',
-    ghost: '#561D25',
-    bug: '#A2FAA3',
-    poison: '#795663',
-    ground: '#D2B074',
-    dragon: '#DA627D',
-    steel: '#1D8A99',
-    fighting: '#2F2F2F',
-    default: '#2A1A1F',
-}
+const colors = {
+  bug: "#90ee92",
+  default: "#2A1A1F",
+  dragon: "#db5572",
+  electric: "#f3da4e",
+  fighting: "#353434",
+  fire: "#f33224",
+  flying: "#69e4bf",
+  ghost: "#410910",
+  grass: "#2ba784",
+  ground: "#b69459",
+  ice: "#8fe1fc",
+  normal: "#9c868a",
+  poison: "#855667",
+  psychic: "#fca9c5",
+  rock: "#858085",
+  steel: "#148c9c",
+  water: "#0598c9",
+};
 
 // Searchbar's function to access the Pokemon API
-const searchPokemon = (event) =>{
-    event.preventDefault();
-    const { value } = event.target.pokemon;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
-    .then(data => data.json())
+const searchPokemon = (event) => {
+  event.preventDefault();
+  const { value } = event.target.pokemon;
+  fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+    .then((data) => data.json())
     // Calling function to render Pokémon
-    .then(response => renderPokemonData(response))
+    .then((response) => renderPokemonData(response))
     // Calling function for the invalid input
-    .catch(err => renderNotFound())
-}
+    .catch((err) => renderNotFound());
+};
 
-let renderPokemonData = (data) =>{
-    console.log(data)
-    const sprite = data.sprites.front_default
-    const { stats, types } = data;
-    
-    // Retrieving data
-    pokeId.textContent = `Nº ${data.id}`
-    pokeImg.setAttribute('src', sprite);     
-    pokeName.textContent = data.name;
-    pokeHeight.textContent = `Height: ${data.height/10} m`
-    pokeWeight.textContent = `Weight: ${data.weight/10} kg`
-    // setCardColor(types);
-    // renderPokemonTypes(types);
-    // renderPokemonStats(stats);
-}
+const renderPokemonData = (data) => {
+  console.log(data);
+  const sprite = data.sprites.front_default;
+  const { stats, types } = data;
 
-// let setCardColor = (types) => {
-//     const colorOne = typeColors[types[0].type.name];
-// }
+  // Retrieving data
+  pokeId.textContent = `Nº ${data.id}`;
+  pokeImg.setAttribute("src", sprite);
+  pokeName.textContent = data.name;
+  pokeHeight.textContent = `Height: ${data.height / 10} m`;
+  pokeWeight.textContent = `Weight: ${data.weight / 10} kg`;
+  setCardColor(types);
+  renderPokemonTypes(types);
+  renderPokemonStats(stats);
+};
+
+const setCardColor = (types) => {
+    console.log("setting color based on types")
+  const colorOne = colors[types[0].type.name];
+  const colorTwo = types[1] ? colors[types[1].type.name] : colors.default;
+  pokeImg.style.background = `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+  pokeImg.style.backgroundSize = "2.5px 2.5px";
+};
+
+const renderPokemonTypes = (types) => {
+    console.log("displaying types")
+  pokeTypes.innerHTML = "";
+  types.forEach((type) => {
+    const typeTextElement = document.createElement("div");
+    typeTextElement.style.color = colors[type.type.name];
+    typeTextElement.textContent = type.type.name;
+    pokeTypes.appendChild(typeTextElement);
+  });
+};
+
+const renderPokemonStats = (stats) => {
+    console.log("doing stats")
+}
 
 //Function to render error
 const renderNotFound = () => {
-    pokeId.textContent = '';
-    pokeName.textContent = '?... Not found... Who\'s that Pokemon?';
-    pokeImg.setAttribute('src', 'pokeMissing.png');
-    pokeImg.style.background =  '#fff';
-    pokeTypes.innerHTML = '';
-    pokeHeight.textContent = '';
-    pokeWeight.textContent = '';
-    pokeStats.innerHTML = '';
-}
+  pokeId.textContent = "";
+  pokeName.textContent = "?... Not found... Who's that Pokemon?";
+  pokeImg.setAttribute("src", "pokeMissing.png");
+  pokeImg.style.background = "#fff";
+  pokeTypes.innerHTML = "";
+  pokeHeight.textContent = "";
+  pokeWeight.textContent = "";
+  pokeStats.innerHTML = "";
+};
